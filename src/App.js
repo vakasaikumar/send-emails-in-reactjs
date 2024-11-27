27d2c6c4dedba0116418f2a2e9b2a06d31de30b0
vakasaikumar <105001957+vakasaikumar@users.noreply.github.com>
2024-11-27 10:22:57 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,57 @@
-import React,{useState} from "react";
+import React, { useState } from 'react';
+import './App.css';
 
-const App = () =>{
-    const[date,setDate] = useState({
-        username:"",
-        password:""
-    })
+const App = () => {
+    const [data, setData] = useState({
+        username: '',
+        email: '',
+        password: '',
+        confirmpassword: ''
+    });
 
-    const {username,password} = date;
-    const changeHandler = ele =>{
-        setDate({...date,[ele.target.name]:[ele.target.value]})
-    }
-    const submitHandler = ele =>{
-        ele.preventDefault()
-        console.log(date)
-    }
-    return(
-        <div>
-            <center>
-                <form onSubmit={submitHandler}>
-                    <input type="text" name="username" value={username} onChange={changeHandler}/><br/><br/>
-                    <input type="password" name="password" value={password} onChange={changeHandler}/><br/><br/>
-                    <input type="submit" name="submit"/>
-                </form>
-            </center>
+    const { username, email, password, confirmpassword } = data;
+
+    const changeHandler = (ele) => {
+        setData({ ...data, [ele.target.name]: ele.target.value });
+    };
+
+    const submitHandler = (ele) => {
+        ele.preventDefault();
+        if (password === confirmpassword) {
+            console.log(data);
+        } else {
+            console.log('Password is not matching');
+        }
+    };
+
+    return (
+        <div className="form-container">
+            <h2>Register</h2>
+            <form onSubmit={submitHandler}>
+                <div className="form-group">
+                    <label htmlFor="username">Username:</label>
+                    <input type="text" id="username" name="username" value={username} onChange={changeHandler} />
+                </div>
+
+                <div className="form-group">
+                    <label htmlFor="email">Email:</label>
+                    <input type="email" id="email" name="email" value={email} onChange={changeHandler} />
+                </div>
+
+                <div className="form-group">
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" id="password" name="password" value={password} onChange={changeHandler} />
+                </div>
+
+                <div className="form-group">
+                    <label htmlFor="confirmpassword">Confirm Password:</label>
+                    <input type="password" id="confirmpassword" name="confirmpassword" value={confirmpassword} onChange={changeHandler} />
+                </div>
+
+                <button type="submit" className="submit-button">Register</button>
+            </form>
         </div>
-    )
-}
-export default App
+    );
+};
+
+export default App;
