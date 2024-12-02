38c0db1613c6562548ac76f908627736ec4bff9a
vakasaikumar <105001957+vakasaikumar@users.noreply.github.com>
2024-12-02 13:03:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,35 +1,70 @@
-import React,{useState} from "react";
-import axios from "axios";
+import React, { useState } from 'react';
+import emailjs from 'emailjs-com'; //npm install emailjs-com 
 
-const App = () =>{
-  const[data,setData] = useState({
-    fullname:'',
-    email:'',
-    password:'',
-    confirmpassword:''
-  })
-  const{fullname,email,password,confirmpassword} = data;
-  const changeHandler = ele=>{
-    setData({...data,[ele.target.name]:ele.target.value})
-  }
-  const submitHandler = ele =>{
+const App = () => {
+  const [data, setData] = useState({
+    subject: '',
+    email: '',
+    message: ''
+  });
+
+  const { subject, email, message } = data;
+
+  const changeHandler = (ele) => {
+    setData({ ...data, [ele.target.name]: ele.target.value });
+  };
+
+  const submitHandler = (ele) => {
     ele.preventDefault();
-    axios.get('https://vskdev-6670a-default-rtdb.firebaseio.com/register.json',
-      data).then(()=> alert('Submited Successfully'))
-    //console.log(data)
-  }
-  return(
+
+    emailjs.sendForm(   // copy this code in emailjs in chorme('https://www.emailjs.com/docs/examples/reactjs/') 
+      'service_rkdw1x4',  // this all details available in ("https://dashboard.emailjs.com/admin")
+      'template_2hbhk4n',
+      ele.target, // Pass the form element
+      'wWVxgN4d2dDUf2XQX' // Public key
+    )
+    .then(
+      () => {
+        console.log('SUCCESS!');
+      },
+      (error) => {
+        console.log('FAILED...', error.text);
+      }
+    );
+
+    ele.target.reset();
+  };
+
+  return (
     <div>
       <center>
-        <form onSubmit={submitHandler}> 
-          <input type="text" name="fullname" value={fullname} onChange={changeHandler}/><br/>
-          <input type="email" name="email" value={email} onChange={changeHandler}/><br/>
-          <input type="password" name="password" value={password} onChange={changeHandler}/><br/>
-          <input type="password" name="confirmpassword" value={confirmpassword} onChange={changeHandler}/><br/>
-          <input type="submit" name="submit"/>
+        <form onSubmit={submitHandler} autoComplete="off">
+          <label>Subject:</label><br />
+          <input
+            type="text"
+            name="subject"
+            value={subject}
+            onChange={changeHandler}
+          /><br />
+          <label>Email:</label><br />
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={changeHandler}
+          /><br />
+          <label>Message:</label><br />
+          <input
+            type="text"
+            name="message"
+            value={message}
+            onChange={changeHandler}
+          /><br /><br />
+          <input type="submit" name="submit" />
         </form>
       </center>
     </div>
-  )
-}
+  );
+};
+
 export default App;
